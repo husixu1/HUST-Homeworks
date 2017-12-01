a246c33c9d4d52501be39c9878d2c0eec0554c13
HuSixu@Archlinux <husixu1@hotmail.com>
2017-12-01 14:24:07 +0800

    operatingSystem updated

---
+++ operatingSystem/ref/sem.c
@@ -0,0 +1,63 @@
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<stdio.h>
+
+union semun {
+    int              val;    /* Value for SETVAL */
+    struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+    unsigned short  *array;  /* Array for GETALL, SETALL */
+    struct seminfo  *__buf;  /* Buffer for IPC_INFO */
+};
+
+int create_Sem(int key, int size) {
+    int id;
+    id = semget(key, size, IPC_CREAT | 0666);           //创建size个信号量
+    if (id < 0) {                                       //判断是否创建成功
+        printf("create sem %d,%d error\n", key, size);  //创建失败，打印错误
+    }
+    return id;
+}
+
+void destroy_Sem(int semid) {
+    int res = semctl(semid, 0, IPC_RMID, 0);            //从系统中删除信号量
+    if (res < 0) {                                      //判断是否删除成功
+        printf("destroy sem %d error\n", semid);        //信号量删除失败，输出信息
+    }
+    return;
+}
+
+int get_Sem(int key, int size) {
+    int id;
+    id = semget(key, size, 0666);                       //获取已经创建的信号量
+    if (id < 0) {
+        printf("get sem %d,%d error\n", key, size);
+    }
+    return id;
+}
+
+void set_N(int semid, int index, int n) {
+    union semun semopts;
+    semopts.val = 1;                                    //设定SETVAL的值为1
+    semctl(semid, index, SETVAL, semopts);              //初始化信号量，信号量编号为0
+    return;
+}
+
+void P(int semid, int index) {
+    struct sembuf sem;                                  //信号量操作数组
+    sem.sem_num = index;                                //信号量编号
+    sem.sem_op = -1;                                    //信号量操作，-1为P操作
+    sem.sem_flg = 0;                                    //操作标记：0或IPC_NOWAIT等
+    semop(semid, &sem, 1);                              //1:表示执行命令的个数
+    return;
+}
+
+void V(int semid, int index) {
+    struct sembuf sem;                                  //信号量操作数组
+    sem.sem_num = index;                                //信号量编号
+    sem.sem_op =  1;                                    //信号量操作，1为V操作
+    sem.sem_flg = 0;                                    //操作标记
+    semop(semid, &sem, 1);                              //1:表示执行命令的个数
+    return;
+}
+
